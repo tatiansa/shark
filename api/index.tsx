b2824eebec570f0b929af09ebddb5b598b8b255d
Ivanov <m.ivanov@bars.group>
2024-08-13 23:58:50 +0700

    ready

--- api/index.tsx
+++ api/index.tsx
@@ -2,74 +2,23 @@ import { Button, Frog } from "@airstack/frog";
 import { devtools } from "@airstack/frog/dev";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { handle } from "@airstack/frog/vercel";
-import { config } from "dotenv";
-
-config();
 
 export const app = new Frog({
     apiKey: process.env.AIRSTACK_API_KEY as string,
-    assetsPath: "/",
-    basePath: "/api",
+    assetsPath: "/", // Указание директории для статических файлов
+    basePath: "/api", // Указание базового пути для API
 });
 
-// Frame to display initial image and button
+// Обработчик для основного маршрута
 app.frame('/', (c) => {
-    return c.res({
-        action: '/smile',
-        image: (
-            <div
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            >
-                <img
-                    src="/images/img1.jpg"
-                    alt="Initial Image"
-                    style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}
-                />
-            </div>
-        ),
-        intents: [
-            <Button value="smile">Click to make Sharky smile</Button>
-        ]
-    });
-});
-
-// Frame to handle smile button click and display the second image
-app.frame('/smile', (c) => {
     const { buttonValue } = c;
-    const imageSrc = buttonValue === "smile" ? "/images/img2.jpg" : "/images/img1.jpg";
+    const imageSrc = buttonValue === "smile" ? `${app.assetsPath}img2.jpg` : `${app.assetsPath}img1.jpg`;
 
     return c.res({
-        image: (
-            <div
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            >
-                <img
-                    src={imageSrc}
-                    alt="Response Image"
-                    style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}
-                />
-            </div>
-        )
+        image: imageSrc, // Передаем URL изображения
+        intents: [
+            <Button value="smile">Click to make Sharky smile</Button> // Кнопка для смены изображения
+        ]
     });
 });
 
